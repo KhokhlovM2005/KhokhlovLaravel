78fd3ef80d08f37c7bea2dfa5bede96c20cd975c
KhokhlovM2005 <mhohlov545@gmail.com>
2024-01-21 18:55:04 +0300

    Запуск 3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//задание 9.1
+Route::get('/user/{id?}', function ($id=0) {
+    return $id;
+});
